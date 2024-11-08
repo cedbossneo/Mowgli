6baf2df45f464389988c7295b80c8686b572abdb
Pepeuch <44699883+Pepeuch@users.noreply.github.com>
2024-11-08 01:19:36 +0100

    Update main.h

--- stm32/custom_panel_fw/YF500C_panel/include/main.h
+++ stm32/custom_panel_fw/YF500C_panel/include/main.h
@@ -28,7 +28,7 @@ extern "C" {
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_hal.h"
-
+#include <stdbool.h> 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
