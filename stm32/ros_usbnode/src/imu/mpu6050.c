30ae5368b7ced26217ce7415d44c29be401f558b
quelqundev <quelqun007@gmail.com>
2026-04-27 07:12:48 +0200

    Disable Low Power Mode

--- stm32/ros_usbnode/src/imu/mpu6050.c
+++ stm32/ros_usbnode/src/imu/mpu6050.c
@@ -32,35 +32,14 @@ uint8_t MPU6050_TestDevice(void)
 
 void MPU6050_Init(void)
 {
-  /**
-   * The MPU-60X0 can be put into Low Power Mode by setting CYCLE to 1 in the Power Management 1 register (Register 107). In this mode, the device will power off all devices except for the primary I2C interface, waking only the accelerometer at fixed intervals to take a single measurement. The frequency of wake-ups can be configured with LP_WAKE_CTRL as shown below:
-   *
-   * LP_WAKE_CTRL | Wake-up Frequency
-   * -------------+------------------
-   * 0            | 1.25 Hz
-   * 1            | 2.5 Hz
-   * 2            | 20 Hz
-   * 3            | 40 Hz
-   */
-
   /** PWR_MGMT_1 register :
-  SLEEP to 0 : sleep mode disabled
-  CYCLE to 1 : low power mode with period wake up enabled
-  TMP_DIS to 1 : disable temperature sensor
-  CLKSEL[1:0] to 0 : use oscillator clock
+  SLEEP[6] to 0 : sleep mode disabled
+  CYCLE[5] to 0 : accelerometer-only low power mode disabled
+  TMP_DIS[3] to 1 : disable temperature sensor
+  CLKSEL[2:0] to 1 : use PLL on X axis of gyro clock
   */
-  SW_I2C_UTIL_WRITE(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1, 0b00101000);
+  SW_I2C_UTIL_WRITE(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_1, 0b00001001);  //SLEEP reset to WAKE;
 
-  /** PWR_MGMT_2 register
-  LP_WAKE_CTRL[7:6] set to 2 (20Hz). Specifies to wake up at 20 Hz during low power mode
-  STBY_XA When set to 1, this bit puts the X axis accelerometer into standby mode.
-  STBY_YA When set to 1, this bit puts the Y axis accelerometer into standby mode.
-  STBY_ZA When set to 1, this bit puts the Z axis accelerometer into standby mode.
-  STBY_XG When set to 1, this bit puts the X axis gyroscope into standby mode.
-  STBY_YG When set to 1, this bit puts the Y axis gyroscope into standby mode.
-  STBY_ZG When set to 1, this bit puts the Z axis gyroscope into standby mode.
-  */
-  SW_I2C_UTIL_WRITE(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_PWR_MGMT_2, 0b10111111);
 
   /** CONFIG register :
   DLPF_CFG[2:0] set to 5 : Low pass filter 10 Hz
@@ -68,7 +47,7 @@ void MPU6050_Init(void)
   SW_I2C_UTIL_WRITE(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_CONFIG, 0x5);
 
   /** SMPLRT_DIV register :
-   SMPLRT_DIV[7:0] set to 9 : sample rate divider 10 (=> 1 kHz/(9+1) = 100 Hz)
+   SMPLRT_DIV[7:0] set to 9 : sample rate divider 10 (=> 40 Hz/(0+1) = 100 Hz)
   */
   SW_I2C_UTIL_WRITE(MPU6050_DEFAULT_ADDRESS, MPU6050_RA_SMPLRT_DIV, 9);
 
